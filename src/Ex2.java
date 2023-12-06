ced35321f9933571d7177ff1717478f6d0271c53
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-06 17:04:03 +0200

    Create Ex2.java

---
+++ src/Ex2.java
@@ -0,0 +1,24 @@
+public class Ex2 {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new MyThread3());
+        Thread thread2 = new Thread(new MyThread4());
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class MyThread3 implements Runnable {
+    public void run() {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
+
+class MyThread4 implements Runnable {
+    public void run() {
+        for (int i = 100; i > 0; i--) {
+            System.out.println(i);
+        }
+    }
+}
