0c326fc221a33ed68f5f87f0a0661ce667005c35
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-06 17:12:28 +0200

    Update Ex2.java

--- src/Ex2.java
+++ src/Ex2.java
@@ -1,24 +1,36 @@
-public class Ex2 {
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(new MyThread3());
-        Thread thread2 = new Thread(new MyThread4());
-        thread1.start();
-        thread2.start();
-    }
-}
-
-class MyThread3 implements Runnable {
+public class Ex2 extends Thread {
     public void run() {
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
     }
-}
 
-class MyThread4 implements Runnable {
-    public void run() {
+    public static void main(String[] args) {
+        Ex2 thread1 = new Ex2();
+        thread1.start();
         for (int i = 100; i > 0; i--) {
             System.out.println(i);
         }
     }
+//        Thread thread1 = new Thread(new MyThread3());
+//        Thread thread2 = new Thread(new MyThread4());
+//        thread1.start();
+//        thread2.start();
+//    }
+//}
+//
+//class MyThread3 implements Runnable {
+//    public void run() {
+//        for (int i = 1; i <= 100; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//}
+//
+//class MyThread4 implements Runnable {
+//    public void run() {
+//        for (int i = 100; i > 0; i--) {
+//            System.out.println(i);
+//        }
+//    }
 }
