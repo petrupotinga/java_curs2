40ac1f5d9d76f7094eb9f487c08da4a47830a2df
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-07 17:55:18 +0200

    Update Ex1.java

--- src/Ex1.java
+++ src/Ex1.java
@@ -1,25 +1,34 @@
-public class Ex1 {
-    public static void main(String[] args) {
-        MyThread1 myThread1 = new MyThread1();
-        MyThread2 myThread2 = new MyThread2();
-        myThread1.start();
-        myThread2.start();
-    }
-}
-
-class MyThread1 extends Thread {
+public class Ex1 extends Thread{
     public void run() {
         for (int i = 1; i <= 100; i++) {
             System.out.println(i);
         }
     }
-}
+        public static void main (String[]args){
+            Ex1 thread1 = new Ex1();
+            thread1.start();
 
-class MyThread2 extends Thread {
-    public void run() {
-        for (int i = 100; i > 0; i--) {
-            System.out.println(i);
+            for (int i = 100; i > 0; i--) {
+                System.out.println(i);
+            }
         }
     }
-}
+
+
+
+//class MyThread1 extends Thread {
+//    public void run() {
+//        for (int i = 1; i <= 100; i++) {
+//            System.out.println(i);
+//        }
+//    }
+//}
+//
+//class MyThread2 extends Thread {
+//    public void run() {
+//        for (int i = 100; i > 0; i--) {
+//            System.out.println(i);
+//        }
+//    }
+//}
 
