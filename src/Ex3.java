a72996c31d9f8afb623b791b4989cf8a6f2b7975
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-07 18:13:51 +0200

    Create Ex3.java

---
+++ src/Ex3.java
@@ -0,0 +1,11 @@
+public class Ex3 {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("Privet");
+            }
+        }).start();
+        new Thread(() -> System.out.println("privet")).start();
+    }
+}
