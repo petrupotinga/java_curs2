1b00deb1192588a0521409decff2ee88f2761704
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-08 18:30:37 +0200

    Update Ex4.java

--- src/Ex4.java
+++ src/Ex4.java
@@ -1,11 +1,11 @@
 public class Ex4 {
     public static void main(String[] args) {
         MyThread4 myThread4 = new MyThread4();
+        myThread4.setName("moy potok ");
+        myThread4.setPriority(9);
         System.out.println("Name of myThread4 = " + myThread4.getName() +
                 "Priority of myThread4 = " + myThread4.getPriority());
-        MyThread4 myThread5 = new MyThread4();
-        System.out.println("Name of myThread5 = " + myThread5.getName() +
-                "Priority of myThread5 = " + myThread5.getPriority());
+
     }
 }
 class MyThread4 extends Thread{
