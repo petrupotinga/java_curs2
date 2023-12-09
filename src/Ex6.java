6df26417aa879302c2c662dce16ca6d6527e9b26
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-09 21:21:20 +0200

    Create Ex6.java

---
+++ src/Ex6.java
@@ -0,0 +1,8 @@
+public class Ex6 {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 5; i > 0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+    }
+}
