10d0181c485904bbd51b5b99029b96936587ef3d
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-10 17:00:40 +0200

    Create EX8.java

---
+++ src/EX8.java
@@ -0,0 +1,23 @@
+public class EX8 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("method main begins");
+        Thread thread = new Thread(new Worker());
+        thread.start();
+        thread.join();
+        System.out.println("method main ends");
+    }
+}
+
+class Worker implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("work begins");
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("work ends");
+    }
+}
