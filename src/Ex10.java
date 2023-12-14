32de83cdb6816660f2641a5632b7b2fd8f2af4ff
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-14 18:45:25 +0200

    Create Ex10.java

---
+++ src/Ex10.java
@@ -0,0 +1,28 @@
+public class Ex10 {
+    static int counter = 0;
+
+    public static void increment() {
+        counter++;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new R());
+        Thread thread2 = new Thread(new R());
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+        System.out.println(counter);
+
+    }
+}
+
+class R implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            Ex10.increment();
+        }
+    }
+}
+
