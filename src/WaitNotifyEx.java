9ca38bf3268848c82fe4de51b50623851a9ca980
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-18 15:39:46 +0200

    Create WaitNotifyEx.java

---
+++ src/WaitNotifyEx.java
@@ -0,0 +1,72 @@
+public class WaitNotifyEx {
+    public static void main(String[] args) {
+        Market market = new Market();
+        Producer producer = new Producer(market);
+        Consumer consumer = new Consumer(market);
+        Thread thread1 = new Thread(producer);
+        Thread thread2 = new Thread(consumer);
+        thread1.start();
+        thread2.start();
+    }
+}
+
+class Market {
+    private int breadCount = 0;
+
+    public synchronized void getBread() {
+        while (breadCount < 1) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        breadCount--;
+        System.out.println("Cumparatorul a cumparat 1 piine");
+        System.out.println("Cantitatea de piine in magazin " + breadCount);
+        notify();
+    }
+
+    public synchronized void putBread() {
+        while (breadCount >= 5) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Producatorul a adaugat la vitrina 1 piine");
+        System.out.println("Cantitatea de piine in magazin " + breadCount);
+        notify();
+    }
+}
+
+class Producer implements Runnable {
+    Market market;
+
+    Producer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            market.putBread();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    Market market;
+
+    Consumer(Market market) {
+        this.market = market;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i <= 10; i++) {
+            market.getBread();
+        }
+    }
+}
