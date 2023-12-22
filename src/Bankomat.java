2814727fc81c473d30eeb33eae4e6e20052e7ec6
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-22 18:00:46 +0200

    Create Bankomat.java

---
+++ src/Bankomat.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Bankomat {
+    public static void main(String[] args) {
+        Lock lock = new ReentrantLock();
+        new Employee("Zaur", lock);
+        new Employee("Oleg", lock);
+        new Employee("Elena", lock);
+        new Employee("Victor", lock);
+        new Employee("Marina", lock);
+    }
+}
+
+class Employee extends Thread {
+    String name;
+    private Lock lock;
+
+    public Employee(String name, Lock lock) {
+        this.name = name;
+        this.lock = lock;
+        this.start();
+    }
+
+    public void run() {
+        System.out.println(name + " asteapta... ");
+        lock.lock();
+        System.out.println(name + " foloseste bancomatul");
+        try {
+            Thread.sleep(2000);
+            System.out.println(name + " a finalizat tranzactia");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+}
