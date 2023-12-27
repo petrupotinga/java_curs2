10b36069174907f830e35836a9537b6bc7298049
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-27 16:56:25 +0200

    Update ThreadPoolEx1.java

--- src/ThreadPoolEx1.java
+++ src/ThreadPoolEx1.java
@@ -3,12 +3,13 @@ import java.util.concurrent.Executors;
 
 public class ThreadPoolEx1 {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(5);
+//        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
         for (int i = 1; i <= 10; i++) {
             executorService.execute(new RunnableImpl100());
         }
         executorService.shutdown();
-        System.out.println("Main ends");
+//        System.out.println("Main ends");
     }
 }
 
@@ -16,11 +17,13 @@ class RunnableImpl100 implements Runnable {
 
     @Override
     public void run() {
-        System.out.println(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + " begins work");
         try {
-            Thread.sleep(500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+        System.out.println(Thread.currentThread().getName() + " ends work");
+
     }
 }
