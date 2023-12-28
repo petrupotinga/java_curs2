3b5f4ea06e862d7be38d11198979ea63a5d8770b
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2023-12-28 19:03:21 +0200

    Create ThreadPoolEx2.java

---
+++ src/ThreadPoolEx2.java
@@ -0,0 +1,33 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+
+
+public class ThreadPoolEx2 {
+    public static void main(String[] args) {
+        ScheduledExecutorService scheduledExecutorService =
+                Executors.newScheduledThreadPool(1);
+//        for (int i = 1; i <= 10; i++) {
+//            scheduledExecutorService.execute(new RunnableImpl200());
+//        }
+        scheduledExecutorService.schedule(new RunnableImpl200()
+        , 3, TimeUnit.SECONDS);
+        scheduledExecutorService.shutdown();
+
+    }
+}
+class RunnableImpl200 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println(Thread.currentThread().getName() + " begins work");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + " ends work");
+
+    }
+}
