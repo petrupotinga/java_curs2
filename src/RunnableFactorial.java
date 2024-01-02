9e6c6da3b525616b345f7db33c7c08b9930a1473
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-02 16:27:53 +0200

    Update RunnableFactorial.java

--- src/RunnableFactorial.java
+++ src/RunnableFactorial.java
@@ -1,16 +1,17 @@
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 public class RunnableFactorial {
     static int factorialResult;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ExecutorService executorService = Executors.newSingleThreadExecutor();
         Factorial factorial = new Factorial(5);
-        executorService.execute(factorial);
+//        executorService.execute(factorial);
+        Future future = executorService.submit(factorial);
         executorService.shutdown();
         executorService.awaitTermination(10, TimeUnit.SECONDS);
+        System.out.println(future.get());
+        System.out.println(future.isDone());
         System.out.println(factorialResult);
     }
 }
