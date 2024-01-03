d5abb99bc08f98cd079aaf31f3d37b288a9cd169
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-03 15:15:07 +0200

    Create SemaphoreEx.java

---
+++ src/SemaphoreEx.java
@@ -0,0 +1,38 @@
+import java.util.concurrent.Semaphore;
+
+public class SemaphoreEx {
+    public static void main(String[] args) {
+        Semaphore callBox = new Semaphore(2);
+        new Person("Zaur", callBox);
+        new Person("Oleg", callBox);
+        new Person("Elena", callBox);
+        new Person("Victor", callBox);
+        new Person("Maria", callBox);
+    }
+}
+
+class Person extends Thread {
+    String name;
+    private Semaphore callBox;
+
+    public Person(String name, Semaphore callBox) {
+        this.name = name;
+        this.callBox = callBox;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            System.out.println(name + " asteapta ...");
+            callBox.acquire();
+            System.out.println(name + " se foloseste de telefon");
+            sleep(2000);
+            System.out.println(name + " a finisat apelul");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            callBox.release();
+        }
+    }
+}
+
