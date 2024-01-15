809c71345ce57488c9ef96b168f9e532a48883cf
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-15 15:27:20 +0200

    Update FileWriterEx.java

--- src/work_with_files/FileWriterEx.java
+++ src/work_with_files/FileWriterEx.java
@@ -6,14 +6,19 @@ import java.io.IOException;
 public class FileWriterEx {
     public static void main(String[] args) throws IOException {
         String rubai = """
-                Privet!
-                Salut!""";
+                Палец, писавший, движется вперед;
+                И, написанное оставив впереди,
+                Не отмоешь слезами, не стереть,
+                И не вернуть назад его слова.""";
+        String s = "Felicitari!";
         FileWriter writer = null;
         try {
-            writer = new FileWriter("test2.txt");
-            for (int i = 0; i < rubai.length(); i++) {
-                writer.write(rubai.charAt(i));
-            }
+            writer = new FileWriter("test2.txt", true);
+//            for (int i = 0; i < rubai.length(); i++) {
+//                writer.write(rubai.charAt(i));
+//            }
+//            writer.write(rubai);
+            writer.write(s);
             System.out.println("Done!");
         } catch (IOException e) {
             e.printStackTrace();
