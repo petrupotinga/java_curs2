fab6d59c1ea94ea3370c2d78669bfe7ca36962e9
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-16 17:05:02 +0200

    Am modificat CopyEx (readLine())

--- src/work_with_files/CopyEx.java
+++ src/work_with_files/CopyEx.java
@@ -6,10 +6,16 @@ public class CopyEx {
     public static void main(String[] args) {
         try (BufferedReader reder = new BufferedReader(new FileReader("test2.txt"));
              BufferedWriter writer = new BufferedWriter(new FileWriter("test3.txt"))) {
-            int character;
-            while ((character = reder.read()) != -1) {
-                writer.write(character);
+//            int character;
+//            while ((character = reder.read()) != -1) {
+//                writer.write(character);
+//            }
+            String line;
+            while ((line = reder.readLine()) != null) {
+                writer.write(line);
+                writer.write('\n');
             }
+            System.out.println("Done!");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
