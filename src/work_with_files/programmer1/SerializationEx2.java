0cc3188da49431314869be37acb99fcf54ed2a3e
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-18 14:48:34 +0200

    Create SerializationEx2.java

---
+++ src/work_with_files/programmer1/SerializationEx2.java
@@ -0,0 +1,21 @@
+package work_with_files.programmer1;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class SerializationEx2 {
+    public static void main(String[] args) {
+        Car car = new Car("Renault", "Megane");
+        Employee employee = new Employee("Maria", "IT", 28, 500, car);
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream("employees2.bin"))) {
+            outputStream.writeObject(employee);
+            System.out.println("Done!");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
