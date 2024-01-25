a11c43249e8934b2a6b3aa06d3f281f0da6e8f74
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-25 16:56:36 +0200

    Create CopyFileTreeEx.java

---
+++ src/work_with_files/CopyFileTreeEx.java
@@ -0,0 +1,39 @@
+package work_with_files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class CopyFileTreeEx {
+    public static void main(String[] args) throws IOException {
+        Path source = Paths.get("C:\\Users\\potin\\OneDrive\\Desktop\\X");
+        Path destination = Paths.get("C:\\Users\\potin\\OneDrive\\Desktop\\CopyHere");
+        Files.walkFileTree(source, new MyFileVisitor2(source, destination));
+        System.out.println("Done!");
+
+    }
+}
+
+class MyFileVisitor2 extends SimpleFileVisitor<Path> {
+    Path source;
+    Path destination;
+
+    public MyFileVisitor2(Path source, Path destination) {
+        this.source = source;
+        this.destination = destination;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(dir));
+        Files.copy(dir, newDestination, StandardCopyOption.REPLACE_EXISTING);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        Path newDestination = destination.resolve(source.relativize(file));
+        Files.copy(file, newDestination, StandardCopyOption.REPLACE_EXISTING);
+        return FileVisitResult.CONTINUE;
+    }
+}
