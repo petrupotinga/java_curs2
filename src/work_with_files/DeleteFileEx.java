2ea26b564d3169705c19e80445f5c6f1c8678948
petrupotinga <127047071+petrupotinga@users.noreply.github.com>
2024-01-25 17:12:16 +0200

    Create DeleteFileEx.java

---
+++ src/work_with_files/DeleteFileEx.java
@@ -0,0 +1,29 @@
+package work_with_files;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+public class DeleteFileEx {
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:\\Users\\potin\\OneDrive\\Desktop\\CopyHere");
+        Files.walkFileTree(path, new MyFileVisitor3());
+    }
+}
+
+class MyFileVisitor3 extends SimpleFileVisitor<Path> {
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        System.out.println("Delete file: " + file.getFileName());
+        Files.delete(file);
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        System.out.println("Delete Directory: " + dir);
+        Files.delete(dir);
+        return FileVisitResult.CONTINUE;
+    }
+}
